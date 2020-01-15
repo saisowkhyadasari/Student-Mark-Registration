a17550300cb8aa6f549b8db14c4bb31ef2421e7f
saisowkhyadasari <37112967+saisowkhyadasari@users.noreply.github.com>
2020-01-14 23:01:15 -0800

    Update StudentMarkHigher.java

--- StudentMarkRegistration/src/main/java/org/deloitte/schoolmanagement/studentmarkregistration/beans/StudentMarkHigher.java
+++ StudentMarkRegistration/src/main/java/org/deloitte/schoolmanagement/studentmarkregistration/beans/StudentMarkHigher.java
@@ -1,5 +1,80 @@
-package org.deloitte.schoolmanagement.studentmarkregistration.beans;
+package org.deloitte.sms.studentmarkregistration.beans;
 
-public class StudentMarkHigher {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name="STUDENT_MARK_PRIMARY")
+@Component
+public class StudentMarkPrimary {
+	@Id
+	private int studentid;	
+	@Column(name="ENGLISH_MARK")
+private int english;
+	@Column(name="HINDI_MARK")
+private int hindi;
+	@Column(name="SCIENCE_MARK")
+private int science;
+	@Column(name="MATHS_MARK")
+private int maths;
+	@Column(name="TOTAL")
+private int total;
+	@Column(name="AVERAGE")
+private double average;
+	@Column(name="GRADE")
+private String grade;
+
+public int getStudentid() {
+	return studentid;
+}
+public void setStudentid(int studentid) {
+	this.studentid = studentid;
+}
+public int getEnglish() {
+	return english;
+}
+public void setEnglish(int english) {
+	this.english = english;
+}
+public int getHindi() {
+	return hindi;
+}
+public void setHindi(int hindi) {
+	this.hindi = hindi;
+}
+public int getScience() {
+	return science;
+}
+public void setScience(int science) {
+	this.science = science;
+}
+public int getMaths() {
+	return maths;
+}
+public void setMaths(int maths) {
+	this.maths = maths;
+}
+public int getTotal() {
+	return total;
+}
+public void setTotal(int total) {
+	this.total = total;
+}
+public double getAverage() {
+	return average;
+}
+public void setAverage(double average) {
+	this.average = average;
+}
+public String getGrade() {
+	return grade;
+}
+public void setGrade(String grade) {
+	this.grade = grade;
+}
 
 }
